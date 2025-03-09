8b2e8fdd8b05628f78af670f31d9ae1a701ae404
Himani-sharma007 <himani0342.becse24@chitkara.edu.in>
2025-03-09 22:10:04 +0530

    Solution to Input and Print a Character

--- Input and Print a Character@@6788d5012db25efec946f93b/code.c
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -2,14 +2,12 @@
 #include <stdio.h>
 
 int main() {
-    char ch1 = 'z'; // First test case input, assuming it's 'z'
-    char ch2 = 'A'; // Second test case input, assuming it's 'A'
+    char ch;
+    
     
     // Print the first character
-    printf("%c\n", ch1);
+    scanf("%c\n", ch);
     
-    // Print the second character
-    printf("%c\n", ch2);
     
     return 0;
 }
